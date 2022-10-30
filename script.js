bf2aeed8d38760fdbec79ed4998ce1e353bc82c2
jm-velasquez <jremolcoy@gmail.com>
2022-10-30 01:08:13 -0300

    new

--- script.js
+++ script.js
@@ -1,22 +1,28 @@
 //la variable const es utilizada para evitar que se dupliquen
 //las funciones que no tiene nombre se llaman anonimas.  y se usan como callbacks 
 //dentro un método.
+
+function saludo(){
+    console.log("Hola curso front end");
+}
+
+
 //const saludoNuevo = function() esto ya no se usa, es redundante.  se intodujo un concepto
 //en el 2014 de conceptos de flecha.
 //las funciones de flecha => sirve para indicar que es una función.
 
 //tarea 4
 
-//saludo()
-const n = 1
-
 const saludoNuevo = (numero) => {
-    //condicion de salida 
-    if(numero === 101)
-    console.log("Este es el numero" + numero);
-    saludoNuevo(numero+1)
+    //condicion de salida
+        if(numero < 101){
+        console.log("Este es el numero " + numero);
+        saludoNuevo(numero + 1)
+        
+           
+        }
 }
-
+const n = 1
 saludoNuevo(n)
 
 //Condicionales
---
+++ script3.js
@@ -0,0 +1,36 @@
+var array = ["verduras", "frutas", "cereales"];
+
+var Persona = {
+    nombre: "Cristian",
+    Edad: 21,
+    Anio: 2001,
+};
+
+var personaje = {
+    nombre: "Link",
+    poder: "Fuego",
+    edad: 25,
+};
+
+console.log(array[1]);
+console.log(Persona["Edad"]);
+console.log(
+    "mi personaje es " +
+    personaje["nombre"] +
+    " y su poder es " +
+    personaje["poder"]
+);
+
+var notas = [7, 2, 3, 51];
+
+function suma() {
+    return suma;
+}
+
+function promedio() {
+    var suma2 = suma();
+    var total = suma2 / notas_cantidaddenotas;
+    return total;
+}
+
+promedio();
